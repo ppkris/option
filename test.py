f6256c10ea80ba69f61e1e44b3d3813643246027
ppk <chris_zhu@hotmail.com>
2023-05-28 13:07:15 +0800

    vs code

--- test.py
+++ test.py
@@ -10,3 +10,7 @@ sample_data = [random.randint(1, 100) for _ in range(10)]
 print(sample_data)
 
 # some new code
+def cal_profit_loss_random(opt_st_pr, opt_cost, opt_num, opt_curr_pr):
+    pro_los = opt_curr_pr * opt_num - opt_cost * opt_num
+    print("The profit or loss is", pro_los)
+
